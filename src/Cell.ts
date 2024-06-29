904a617e47e600dde1abd3fdefb66ac3c445003e
chris <c_duckworth@hotmail.com>
2024-06-29 17:43:00 +0100

    add sum property to Shape class

--- src/Cell.ts
+++ src/Cell.ts
@@ -24,3 +24,4 @@ class Cell {
 }
 
 module.exports = Cell;
+export type { Cell };
---
+++ src/Shape.ts
@@ -0,0 +1,21 @@
+import type { Cell } from "./Cell.ts";
+
+class Shape {
+	readonly sum;
+	constructor(sum: number, ...cells: Cell[]) {
+		if (typeof sum !== "number" || Math.floor(sum) !== sum) {
+			throw new TypeError("Sum must be an integer");
+		}
+		// minimum value for n cells is 1 + 2 + ... + n
+		const minSum = (cells.length * (cells.length + 1)) / 2;
+		// max value for n cells is 9 + 8 + ... (9 - n + 1)
+		const maxSum = (-(cells.length - 19) * cells.length) / 2;
+		if (sum < minSum || sum > maxSum) {
+			throw new RangeError("Sum must be valid for number of cells");
+		}
+		this.sum = sum;
+		console.log(cells);
+	}
+}
+
+module.exports = Shape;
