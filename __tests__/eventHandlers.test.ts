2c260b997d7f07ef3f91258f98049cc3b6f0e10e
chris <c_duckworth@hotmail.com>
2024-07-29 18:20:26 +0100

    refactor click event handler to use focus pseudoclass

--- __tests__/eventHandlers.test.ts
+++ __tests__/eventHandlers.test.ts
@@ -4,127 +4,105 @@ import shapes from "./shapes";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 
+function setupTable() {
+  const sudoku = new KillerSudoku(shapes)
+  const table = document.createElement("table")
+  table.classList.add("values")
+  table.appendChild(document.createElement("tbody"))
+  for (let i = 0; i < 9; i++) {
+    const row = document.createElement("tr")
+    for (let j = 0; j < 9; j++) {
+      const td = document.createElement("td")
+      sudoku.cells[9 * i + j].element = td
+      td.addEventListener("click", (e) => {
+        handleCellClick(e, sudoku, 9 * i + j)
+      })
+      row.appendChild(td)
+    }
+    table.firstElementChild!.appendChild(row)
+  }
+
+  return sudoku
+}
+
 describe("handleCellClick", () => {
 	it("should set the currCell property to the clicked cell", async () => {
 		const user = userEvent.setup();
-
-		const sudoku = new KillerSudoku(shapes);
-		const td = document.createElement("td");
-		sudoku.cells[5].element = td;
-		td.addEventListener("click", (e) => {
-			handleCellClick(e, sudoku, 5);
-		});
-
-		await user.click(td);
+		const sudoku = setupTable()
+    
+		await user.click(sudoku.cells[5].element);
 
 		expect(sudoku.currCell).toHaveProperty("position", 5);
 	});
-	it("should add the selected-value class to the clicked cell", async () => {
-		const user = userEvent.setup();
-
-		const sudoku = new KillerSudoku(shapes);
-		const td = document.createElement("td");
-		sudoku.cells[5].element = td;
-		td.addEventListener("click", (e) => {
-			handleCellClick(e, sudoku, 5);
-		});
-
-		await user.click(td);
-
-		expect(td).toHaveClass("selected-value");
-	});
-	it("moving from one cell to the next should remove class from old cell", async () => {
-		const user = userEvent.setup();
-
-		const sudoku = new KillerSudoku(shapes);
-		const td1 = document.createElement("td");
-		const td2 = document.createElement("td");
-		sudoku.cells[5].element = td1;
-		sudoku.cells[45].element = td2;
-		td1.addEventListener("click", (e) => {
-			handleCellClick(e, sudoku, 5);
-		});
-		td2.addEventListener("click", (e) => {
-			handleCellClick(e, sudoku, 45);
-		});
-
-		await user.click(td1);
-		await user.click(td2);
-
-		expect(sudoku.currCell).toHaveProperty("position", 45);
-		expect(td2).toHaveClass("selected-value");
-		expect(td1).not.toHaveClass("selected-value");
-	});
 	it("clicking on the same cell should set notes to true", async () => {
 		const user = userEvent.setup();
+		const sudoku = setupTable()
 
-		const sudoku = new KillerSudoku(shapes);
-		const td = document.createElement("td");
-		sudoku.cells[5].element = td;
-		td.addEventListener("click", (e) => {
-			handleCellClick(e, sudoku, 5);
-		});
-
-		await user.click(td);
-		await user.click(td);
+		await user.click(sudoku.cells[5].element);
+		await user.click(sudoku.cells[5].element);
 
 		expect(sudoku).toHaveProperty("notes", true);
 	});
-	it("clicking on same cell should remove selected-value and add selected-notes", async () => {
+  it("clicking on the same cell should set notes to false if already true", async () => {
 		const user = userEvent.setup();
+		const sudoku = setupTable()
 
-		const sudoku = new KillerSudoku(shapes);
-		const td = document.createElement("td");
-		sudoku.cells[5].element = td;
-		td.addEventListener("click", (e) => {
-			handleCellClick(e, sudoku, 5);
-		});
-
-		await user.click(td);
-		await user.click(td);
-
-		expect(td).not.toHaveClass("selected-value");
-		expect(td).toHaveClass("selected-notes");
-	});
-	it("clicking on same cell again agains should set notes to false and remove selected-notes and add selected-value", async () => {
+		await user.click(sudoku.cells[5].element);
+		await user.click(sudoku.cells[5].element);
+		await user.click(sudoku.cells[5].element);
+    
+    expect(sudoku).toHaveProperty("notes", false)
+  })
+  it("clicking on another cell should not change notes value", async () => {
 		const user = userEvent.setup();
+		const sudoku = setupTable()
 
-		const sudoku = new KillerSudoku(shapes);
-		const td = document.createElement("td");
-		sudoku.cells[5].element = td;
-		td.addEventListener("click", (e) => {
-			handleCellClick(e, sudoku, 5);
-		});
+    await user.click(sudoku.cells[5].element)
+    await user.click(sudoku.cells[5].element)
 
-		await user.click(td);
-		await user.click(td);
-		await user.click(td);
+    await user.click(sudoku.cells[20].element)
+    expect(sudoku).toHaveProperty("notes", true)
+    await user.click(sudoku.cells[20].element)
 
-		expect(sudoku).toHaveProperty("notes", false);
-		expect(td).toHaveClass("selected-value");
-		expect(td).not.toHaveClass("selected-notes");
-	});
-	it("clicking on another cell when notes is true should do change the current cell and switch the classes", async () => {
-		const user = userEvent.setup();
+    await user.click(sudoku.cells[5].element)
+    expect(sudoku).toHaveProperty("notes", false)
+  })
+  it("clicking on the same cell should add the notes class to the table", async () => {
+    const user = userEvent.setup()
+		const sudoku = setupTable()
+    const table = sudoku.cells[0].element.parentElement!.parentElement!.parentElement!
 
-		const sudoku = new KillerSudoku(shapes);
-		const td1 = document.createElement("td");
-		const td2 = document.createElement("td");
-		sudoku.cells[5].element = td1;
-		sudoku.cells[45].element = td2;
-		td1.addEventListener("click", (e) => {
-			handleCellClick(e, sudoku, 5);
-		});
-		td2.addEventListener("click", (e) => {
-			handleCellClick(e, sudoku, 45);
-		});
+    expect(table).toHaveClass("values")
+    expect(table).not.toHaveClass("notes")
 
-		await user.click(td1);
-		await user.click(td1);
-		await user.click(td2);
+    await user.click(sudoku.cells[5].element)
+    await user.click(sudoku.cells[5].element)
 
-		expect(sudoku.currCell).toHaveProperty("position", 45);
-		expect(td2).toHaveClass("selected-notes");
-		expect(td1).not.toHaveClass("selected-notes");
-	});
+    expect(table).toHaveClass("notes")
+    expect(table).not.toHaveClass("values")
+  })
+  it("clicking on the cell once in notes should change to the values class", async () => {
+    const user = userEvent.setup()
+		const sudoku = setupTable()
+    const table = sudoku.cells[0].element.parentElement!.parentElement!.parentElement!
+
+    await user.click(sudoku.cells[5].element)
+    await user.click(sudoku.cells[5].element)
+    await user.click(sudoku.cells[5].element)
+
+    expect(table).toHaveClass("values")
+    expect(table).not.toHaveClass("notes")
+  })
+  it("clicking on a different cell should not change the notes/values classes", async () => {
+    const user = userEvent.setup()
+		const sudoku = setupTable()
+    const table = sudoku.cells[0].element.parentElement!.parentElement!.parentElement!
+
+    await user.click(sudoku.cells[1].element)
+    await user.click(sudoku.cells[5].element)
+    expect(table).toHaveClass("values")
+    await user.click(sudoku.cells[5].element)
+    await user.click(sudoku.cells[2].element)
+    expect(table).toHaveClass("notes")
+  })
 });
--- src/eventHandlers.ts
+++ src/eventHandlers.ts
@@ -5,24 +5,14 @@ function handleCellClick(_e: MouseEvent, sudoku: KillerSudoku, pos: number) {
 	const newCell = sudoku.cells[pos];
 
 	// for when we click on the same cell
-	if (oldCell && oldCell.position === newCell.position) {
-		sudoku.notes = !sudoku.notes;
-		if (sudoku.notes) {
-			oldCell.element.classList.remove("selected-value");
-			oldCell.element.classList.add("selected-notes");
-		} else {
-			oldCell.element.classList.remove("selected-notes");
-			oldCell.element.classList.add("selected-value");
-		}
-		return;
-	}
+  if (oldCell && oldCell.position === newCell.position) {
+    sudoku.notes = !sudoku.notes
+    const table = newCell.element.parentElement!.parentElement!.parentElement!
+    table.classList.add(sudoku.notes ? "notes" : "values")
+    table.classList.remove(sudoku.notes ? "values": "notes")
+  }
 
-	const className = sudoku.notes ? "selected-notes" : "selected-value";
-	if (oldCell !== undefined) {
-		oldCell.element.classList.remove(className);
-	}
 	sudoku.currCell = newCell;
-	newCell.element.classList.add(className);
 	return;
 }
 
